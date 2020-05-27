e7f030c9f31c1d4968bcc91b4bd0c3f39774b860
Carlos Gonzales <carlos.gonzales@jalasoft.com>
2020-05-27 18:50:49 -0400

    create form dynamically

--- src/test/java/org/example/schoology/pages/Courses.java
+++ src/test/java/org/example/schoology/pages/Courses.java
@@ -1,13 +1,26 @@
 package org.example.schoology.pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class Courses {
 
 	private WebDriver driver;
 
+	@FindBy(css = "a.create-course-btn")
+	private WebElement createCourseButton;
+
 	public Courses(WebDriver driver) {
 		this.driver = driver;
+		PageFactory.initElements(driver, this);
+	}
+
+	public CreateCoursePopup clickCreateCourseButton() {
+		createCourseButton.click();
+		return new CreateCoursePopup(driver);
 	}
 
 }
--- src/test/java/org/example/schoology/tests/CoursesTest.java
+++ src/test/java/org/example/schoology/tests/CoursesTest.java
@@ -1,6 +1,11 @@
 package org.example.schoology.tests;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.example.schoology.entities.Course;
 import org.example.schoology.pages.Courses;
+import org.example.schoology.pages.CreateCoursePopup;
 import org.example.schoology.pages.Home;
 import org.example.schoology.pages.Login;
 import org.example.schoology.pages.SubMenu;
@@ -14,8 +19,17 @@ public class CoursesTest {
 		Home home = login.loginAs("carledriss+01@gmail.com", "P@ssw0rd");
 		SubMenu subMenu = home.clickMenu("Courses");
 		Courses courses = subMenu.clickMyCoursesLink();
+		CreateCoursePopup createCoursePopup = courses.clickCreateCourseButton();
+		//Course course = new Course();
+		String courseName = "Test Course";
 
-//		driver.findElement(By.cssSelector("a.create-course-btn")).click();
+		Map<String, String> courseMap = new HashMap<>();
+		courseMap.put("name", courseName);
+		courseMap.put("section", "Section");
+		courseMap.put("area", "Mathematics");
+		courseMap.put("level", "Undergraduate");
+		createCoursePopup.create(courseMap);
+
 //
 //		String courseName = "Test Course";
 //		driver.findElement(By.cssSelector("#edit-course-name")).sendKeys(courseName);
